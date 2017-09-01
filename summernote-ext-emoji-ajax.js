679f421ef21384ed144962cfce3293236323b3ef
tylerecouture <tylerecouture@users.noreply.github.com>
2017-09-01 10:12:54 -0700

    Improve browser responsiveness while loading emoji

--- summernote-ext-emoji-ajax.js
+++ summernote-ext-emoji-ajax.js
@@ -97,17 +97,7 @@
                     // remove the loading icon
                     $('.note-ext-emoji-loading').remove();
 
-                    // insert the emoji buttons
-                    $.each(window.emojiUrls, function (name, url) {
-                        //console.log(index + ": " + value);
-                        $list.append(
-                            '<button type="button" title="' + name + '" ' +
-                            'class="note-emoji-btn btn btn-link" ' +
-                            'tabindex="-1">' +
-                            '    <img src="' + url + '" />' +
-                            '</button>'
-                        );
-                    });
+                    self.populateWithEmojis($list);
 
                     $("button", $list).click(function (event) {
                         var $button = $(this);
@@ -159,6 +149,20 @@
                 context.invoke('editor.focus');
                 context.invoke('editor.insertNode', img);
             };
+            
+            self.populateWithEmojis = function($container) {
+                $.each(window.emojiUrls, function (name, url) {
+                    //console.log(index + ": " + value);
+                    setTimeout(function() { // prevents lag during DOM insertion
+                        $container.append(
+                            '<button type="button" title="' + name + '" ' +
+                            'class="note-emoji-btn btn btn-link" tabindex="-1">' +
+                            '    <img src="' + url + '" />' +
+                            '</button>'
+                        );
+                    }, 0);
+                });
+            }
 
         }
     });
